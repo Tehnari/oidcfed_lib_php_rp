8cefdea1ecb3d73f926cd1da5c014a4ef6012487
Constantin Sclifos <sclifcon@vle.ase.md>
2017-06-02 19:47:44 +0300

    Cleaning and prepare steps for openid-connect federations check/certifications/tests...

--- index.php
+++ index.php
@@ -7,20 +7,34 @@ $oidc->register();
 $cid = $oidc->getClientID();
 $secret = $oidc->getClientSecret();
 //----------
-$oidc->authenticate();
-$oidc->requestUserInfo('sub');
-
-$session = array();
-foreach ($oidc->getUserInfo() as $key => $value) {
-    if (is_array($value)) {
-        $v = implode(', ', $value);
-    }else {
-        $v = $value;
-    }
-        $session[$key] = $v;
-    }
-
-session_start();
-$_SESSION['attributes'] = $session;
+$curl = curl_init('https://rp.certification.openid.net:8080/oidcfed_php_rp/rp-response_type-code/.well-known/openid-configuration');
+curl_setopt($curl, CURLOPT_FAILONERROR, true);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$result_curl = curl_exec($curl);
+$result_pretty_print = json_encode(json_decode($result_curl), JSON_PRETTY_PRINT);
+echo "<pre>";
+//echo $result_curl;
+echo $result_pretty_print;
+echo "</pre>";
+//----------
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+//$oidc->authenticate();
+//$oidc->requestUserInfo('sub');
+//
+//$session = array();
+//foreach ($oidc->getUserInfo() as $key => $value) {
+//    if (is_array($value)) {
+//        $v = implode(', ', $value);
+//    }else {
+//        $v = $value;
+//    }
+//        $session[$key] = $v;
+//    }
+//
+//session_start();
+//$_SESSION['attributes'] = $session;
 
 //header("Location: ./attributes.php");
--- openid_dynamic.php
+++ openid_dynamic.php
@@ -1,27 +1,40 @@
 <?php
-require 'protected/vendor/autoload.php'; 
-
-$issuer = 'https://rp.certification.openid.net:8080/oidcfed_php_rp/rp-response_type-code/token';
+require 'vendor/autoload.php';
+// First testing
+$issuer = 'https://rp.certification.openid.net:8080/oidcfed_lib_php_rp/rp-response_type-code';
+$oidc = new OpenIDConnectClient($issuer);
 $oidc->register();
-$oidc = new OpenIDConnectClient($issuer); 
 $cid = $oidc->getClientID();
-$secret = $oidc->getClientSecret(); 
+$secret = $oidc->getClientSecret();
+//----------
+$curl = curl_init('https://rp.certification.openid.net:8080/oidcfed_php_rp/rp-response_type-code/.well-known/openid-configuration');
+curl_setopt($curl, CURLOPT_FAILONERROR, true);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$result_curl = curl_exec($curl);
+$result_pretty_print = json_encode(json_decode($result_curl), JSON_PRETTY_PRINT);
+echo "<pre>";
+//echo $result_curl;
+echo $result_pretty_print;
+echo "</pre>";
+//----------
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+//$oidc->authenticate();
+//$oidc->requestUserInfo('sub');
+//
+//$session = array();
+//foreach ($oidc->getUserInfo() as $key => $value) {
+//    if (is_array($value)) {
+//        $v = implode(', ', $value);
+//    }else {
+//        $v = $value;
+//    }
+//        $session[$key] = $v;
+//    }
+//
+//session_start();
+//$_SESSION['attributes'] = $session;
 
-$oidc->authenticate(); 
-$oidc->requestUserInfo('sub'); 
-
-$session = array(); 
-foreach ($oidc->getUserInfo() as $key => $value) {
-    if (is_array($value)) {
-        $v = implode(', ', $value); 
-    }else {
-        $v = $value; 
-    }
-        $session[$key] = $v; 
-    }   
-
-session_start(); 
-$_SESSION['attributes'] = $session; 
-
-header("Location: ./attributes.php"); 
-
+//header("Location: ./attributes.php");
