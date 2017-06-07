90aa075caf9ca655241c48b17723c3a9729a911c
Constantin Sclifos <sclifcon@vle.ase.md>
2017-06-07 13:40:46 +0300

    Cleaning and prepare steps for openid-connect federations check/certifications/tests...

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 <?php
+
 require 'vendor/autoload.php';
-// First testing
+// First testing dynaming registration ...
 $issuer = 'https://rp.certification.openid.net:8080/oidcfed_lib_php_rp/rp-response_type-code';
 $oidc = new OpenIDConnectClient($issuer);
 $oidc->register();
 $cid = $oidc->getClientID();
 $secret = $oidc->getClientSecret();
+$oidc_response_obj = new stdClass();
+$oidc_response_obj->rp_id = "oidcfed_lib_php_rp";
+$oidc_response_obj->test_id = "rp-response_type-code";
+$oidc_response_obj->client_id = $cid;
+$oidc_response_obj->client_secret = $secret;
+$result_json = json_encode($oidc_response_obj, JSON_PARTIAL_OUTPUT_ON_ERROR);
+try {
+    file_put_contents('../oidc_dynamic_response.json', $result_json);
+}
+catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+}
+
+$result_json_pretty = json_encode($oidc_response_obj, JSON_PRETTY_PRINT);
+try {
+    file_put_contents('../oidc_dynamic_response_pretty.txt', $result_json_pretty);
+}
+catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+}
+//----------
+// Nest test is rp-response_type-code
+
 //----------
 $curl = curl_init('https://rp.certification.openid.net:8080/oidcfed_php_rp/rp-response_type-code/.well-known/openid-configuration');
 curl_setopt($curl, CURLOPT_FAILONERROR, true);
