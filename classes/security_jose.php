f9b1f4630b6d04dcc9d9a3ab255d7ef9cff35eac
Constantin Sclifos <sclifcon@gmail.com>
2017-07-22 22:03:11 +0300

    PHP CS: Fixing header of hte files (docs)...

--- classes/security_jose.php
+++ classes/security_jose.php
@@ -1,21 +1,29 @@
 <?php
 
 /**
- *
- * @author Constantin Sclifos
- * @copyright (c) 2017, Constantin Sclifos
- * @license    https://opensource.org/licenses/MIT
- *
+ * OIDCFED Library for PHP
+ * 
+ * @abstract OIDCFED Library for PHP
+ * 
+ *  PHP version 5 
+ * 
+ * @category  PHP
+ * @package   OIDCFED_Lib_PHP_RP
+ * @author    Constantin Sclifos <sclifcon@gmail.com>
+ * @copyright 2017 Constantin Sclifos
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @version   "GIT:f23edba8"
+ * @link      https://github.com/Tehnari/oidcfed_lib_php_rp
  * Copyright MIT <2017> Constantin Sclifos <sclifcon@gmail.com>
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
-
-  - The above copyright notice and this permission notice shall be included
+ *
+ *  - The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -25,7 +33,8 @@
  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- *
+ */
+
  */
 
 namespace oidcfed;
