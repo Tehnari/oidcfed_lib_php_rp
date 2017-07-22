d899e94cef6141c041e26289ab59246a8aef8e8e
Constantin Sclifos <sclifcon@gmail.com>
2017-07-22 21:53:20 +0300

    Working with phpcs...

--- openid_dynamic.php
+++ openid_dynamic.php
@@ -1,23 +1,29 @@
 <?php
 
 /**
+ * OIDCFED Library for PHP
+ * 
+ * @abstract OIDCFED Library for PHP
+ * 
+ *  PHP version 5 
+ * 
  * @category  PHP
  * @package   OIDCFED_Lib_PHP_RP
- * @author Constantin Sclifos <sclifcon@gmail.com>
+ * @author    Constantin Sclifos <sclifcon@gmail.com>
  * @copyright 2017 Constantin Sclifos
- * @license    https://opensource.org/licenses/MIT MIT
- * @version "GIT:f23edba8"
- * @link   https://github.com/Tehnari/oidcfed_lib_php_rp
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @version   "GIT:f23edba8"
+ * @link      https://github.com/Tehnari/oidcfed_lib_php_rp
  * Copyright MIT <2017> Constantin Sclifos <sclifcon@gmail.com>
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
-
-    - The above copyright notice and this permission notice shall be included
+ *
+ *  - The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
