3456c863932e59d02a177c0d909a82b79497ec02
Constantin Sclifos <sclifcon@vle.ase.md>
2017-09-03 19:02:15 +0300

    Cleaning... Prepare for tests...

---
+++ tests_oidcfed/rp-sms-mutiple-l0.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *
+ * Level 0
+ *
+ * Test ID:
+ * rp-sms-single-l0
+ *
+ * Description
+ * Make a OpenID Provider Issuer Discovery and interpret the received Signed Metadata Statement. The response will be directly signed by a federation without any intermediate.
+ *
+ * Info
+ * Correctly verified the signature and interpreted the provider information received.
+ *
+ */
+
